dd4d588b3cd96352ca1c13cbbcfdd8e38ba7639e
alisaroberts <hecallsmelili@gmail.com>
2025-09-29 10:52:27 +0000

    fixed bot message

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ export default function App() {
       {/* stars & shooting stars only in cosmic mode */}
       {isCosmic && (
         <>
-          <div className="stars"></div>
-          <div className="shooting-star s1"></div>
-          <div className="shooting-star s2"></div>
-          <div className="shooting-star s3"></div>
+          <div className="stars" />
+          <div className="shooting-star s1" />
+          <div className="shooting-star s2" />
+          <div className="shooting-star s3" />
         </>
       )}
 
       {/* clouds only in day mode */}
       {!isCosmic && (
         <>
-          <div className="cloud cloud1"></div>
-          <div className="cloud cloud2"></div>
+          <div className="cloud cloud1" />
+          <div className="cloud cloud2" />
         </>
       )}
 
@@ -32,7 +32,7 @@ export default function App() {
           </h1>
           <button
             className={`toggle-btn ${isCosmic ? "on" : "off"}`}
-            onClick={() => setIsCosmic(!isCosmic)}
+            onClick={() => setIsCosmic((v) => !v)}
           >
             {isCosmic ? "OFF" : "ON"}
           </button>
@@ -40,9 +40,24 @@ export default function App() {
 
         <div className="chat-body">
           <AstraBot />
-          <div className="bot-message glass" style={{ textAlign: "center" }}>
-            🌌 Have you tried turning the universe off and on again? <br />
-            Thanks for visiting my demo!
+
+          <div className="bot-message glass">
+            <div className="demo-top">
+              🌌 Have you tried turning the universe off and on again?
+            </div>
+
+            <div className="demo-intro">
+              This demo showcases front-end development skills including:
+            </div>
+
+            <ul className="demo-showcase">
+              <li>React component structure with theme toggle</li>
+              <li>CSS animations (stars, asteroid, AstraBot)</li>
+              <li>Glassmorphism UI with cross-browser support</li>
+              <li>Responsive design for mobile and desktop</li>
+            </ul>
+
+            <div className="demo-outro">✨ Thanks for visiting my demo! ✨</div>
           </div>
         </div>
       </div>
@@ -50,7 +65,7 @@ export default function App() {
       {/* floating developer credit asteroid (single GIF only) */}
       <div className="floating-credit">
         <img
-          src={process.env.PUBLIC_URL + "/mydevelopmentcredit.gif"}
+          src={`${process.env.PUBLIC_URL}/mydevelopmentcredit.gif`}
           alt="Alisa Roberts Development 2025"
           className="asteroid"
         />
